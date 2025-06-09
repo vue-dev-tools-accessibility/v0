5846196ebd4f8011b77f8f357e339211f08df0eb
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-06-09 17:03:14 -0400

    Communicate target

--- src/views/AccessibilityViolations.vue
+++ src/views/AccessibilityViolations.vue
@@ -31,83 +31,88 @@
         </span>
         <span class="rule-description">{{ addPeriod(violationGroup.description) }}</span>
       </div>
-      <DoxenAccordion :show="!!violationGroup.show">
-        <div class="rule-details">
-          <span
-            class="pill"
-            :class="violationGroup.impact"
-            :title="_startCase(violationGroup.impact)"
-          >
-            {{ _startCase(violationGroup.impact) }}
-          </span>
-          <span
-            v-for="tag in violationGroup.tags"
-            class="pill gray"
-            :title="tag"
-            :key="tag"
-          >
-            {{ tag }}
-          </span>
-          <div>
-            <span class="rule-id">
-              <strong>ID:</strong>
-              {{ violationGroup.id }}
+      <DoxenAccordion
+        :show="!!violationGroup.show"
+        :key="'accordion-' + violationGroup.id"
+      >
+        <div>
+          <div class="rule-details">
+            <span
+              class="pill"
+              :class="violationGroup.impact"
+              :title="_startCase(violationGroup.impact)"
+            >
+              {{ _startCase(violationGroup.impact) }}
             </span>
-          </div>
-          <div class="rule-help">
-            {{ addPeriod(violationGroup.help) }}
-            <a
-              class="rule-learn-more"
-              :href="violationGroup.helpUrl"
-              target="_blank"
-              :title="urlAsTitle(violationGroup.helpUrl)"
+            <span
+              v-for="tag in violationGroup.tags"
+              class="pill gray"
+              :title="tag"
+              :key="tag"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+              {{ tag }}
+            </span>
+            <div>
+              <span class="rule-id">
+                <strong>ID:</strong>
+                {{ violationGroup.id }}
+              </span>
+            </div>
+            <div class="rule-help">
+              {{ addPeriod(violationGroup.help) }}
+              <a
+                class="rule-learn-more"
+                :href="violationGroup.helpUrl"
+                target="_blank"
+                :title="urlAsTitle(violationGroup.helpUrl)"
               >
-                <title>Learn more</title>
-                <path
-                  d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"
-                  fill="currentColor"
-                />
-              </svg>
-            </a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <title>Learn more</title>
+                  <path
+                    d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"
+                    fill="currentColor"
+                  />
+                </svg>
+              </a>
+            </div>
           </div>
-        </div>
-        <div class="cards">
-          <div
-            v-for="(node, nodeIndex) in violationGroup.nodes"
-            class="card"
-            :key="[key, nodeIndex].join('_')"
-          >
-            <ColorContrastDetails
-              v-if="violationGroup.id === 'color-contrast'"
-              :data="node.any[0].data"
-            />
-            <pre
-              v-else
-              v-text="fixMessage(node.failureSummary)"
-              class="rule-message"
-            ></pre>
+          <div class="cards">
+            <div
+              v-for="(node, nodeIndex) in violationGroup.nodes"
+              class="card"
+              :key="'nodes-' + nodeIndex"
+            >
+              <ColorContrastDetails
+                v-if="violationGroup.id === 'color-contrast'"
+                :data="node.any[0].data"
+              />
+              <pre
+                v-else
+                v-text="fixMessage(node.failureSummary)"
+                class="rule-message"
+              ></pre>
 
-            <button
-              class="rule-highlight"
-              @click="highlightTarget(node.target)"
-            >
-              <svg
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
+              <button
+                class="rule-highlight"
+                @click="highlightTarget(node.target)"
               >
-                <title>target icon</title>
-                <path
-                  d="M22.08,11.04H20.08V4H13.05V2H11.04V4H4V11.04H2V13.05H4V20.08H11.04V22.08H13.05V20.08H20.08V13.05H22.08V11.04M18.07,18.07H13.05V16.06H11.04V18.07H6V13.05H8.03V11.04H6V6H11.04V8.03H13.05V6H18.07V11.04H16.06V13.05H18.07V18.07M13.05,12.05A1,1 0 0,1 12.05,13.05C11.5,13.05 11.04,12.6 11.04,12.05C11.04,11.5 11.5,11.04 12.05,11.04C12.6,11.04 13.05,11.5 13.05,12.05Z"
-                  fill="currentColor"
-                />
-              </svg>
-              Highlight element on page
-            </button>
-            <CodeBlock :code="node.html" />
+                <svg
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>target icon</title>
+                  <path
+                    d="M22.08,11.04H20.08V4H13.05V2H11.04V4H4V11.04H2V13.05H4V20.08H11.04V22.08H13.05V20.08H20.08V13.05H22.08V11.04M18.07,18.07H13.05V16.06H11.04V18.07H6V13.05H8.03V11.04H6V6H11.04V8.03H13.05V6H18.07V11.04H16.06V13.05H18.07V18.07M13.05,12.05A1,1 0 0,1 12.05,13.05C11.5,13.05 11.04,12.6 11.04,12.05C11.04,11.5 11.5,11.04 12.05,11.04C12.6,11.04 13.05,11.5 13.05,12.05Z"
+                    fill="currentColor"
+                  />
+                </svg>
+                Highlight element on page
+              </button>
+              <CodeBlock :code="node.html" />
+            </div>
           </div>
         </div>
       </DoxenAccordion>
@@ -177,7 +182,7 @@ export default {
       return message.trim();
     },
     highlightTarget: function (target) {
-      console.log('STUB: highlightTarget', target[0]);
+      sendToParent(REQUESTS.HIGHLIGHT_TARGET, target[0]);
     },
     runAxe: function () {
       sendToParent(REQUESTS.RUN_AXE);
