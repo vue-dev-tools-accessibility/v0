fae4cfc143bbf3a8a6f194fa1208ff2435c7f10c
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-06-10 08:54:37 -0400

    Title button

--- src/views/AccessibilityViolations.vue
+++ src/views/AccessibilityViolations.vue
@@ -13,6 +13,7 @@
           d="M12 10H6.78A11 11 0 0 1 27 16h2A13 13 0 0 0 6 7.68V4H4v8h8zm8 12h5.22A11 11 0 0 1 5 16H3a13 13 0 0 0 23 8.32V28h2v-8h-8z"
           fill="currentColor"
         />
+        <title>Scan for accessibility issues</title>
       </svg>
     </button>
 
