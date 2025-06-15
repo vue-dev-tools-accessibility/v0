1fc66eb7ba40b2d43c851fd21c13f33d70415800
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2025-06-15 18:22:36 -0400

    Create Settings page

---
+++ src/views/PluginSettings.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">
+    Settings
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PluginSettings'
+};
+</script>
+
+<style scoped>
+.wrapper {
+  padding: 11.25px;
+}
+</style>
