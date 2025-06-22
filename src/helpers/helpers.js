901a2c9f6a7927053f6ff6a9b3c1f66da0b3ac35
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-06-21 23:08:51 -0400

    JSDocs

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -16,12 +16,23 @@ export const upperFirst = function (value) {
   return value[0].toUpperCase() + value.slice(1);
 };
 
+/**
+ * Asynchronously loads components, for better code spliting.
+ *
+ * @param  {Promise} loader  () => import('@/path.file.vue')
+ * @return {object}          A Vue component
+ */
 export const asyncify = function (loader) {
   return defineAsyncComponent({
+    // Async component to load
     loader,
+    // Component to show while loading async component
     loadingComponent: AsyncLoading,
-    delay: 0,
+    // Delay before showing loadingComponent
+    delay: 200,
+    // Component to show if async component times out
     errorComponent: AsyncError,
+    // Time to wait before showing error component
     timeout: 8000
   });
 };
